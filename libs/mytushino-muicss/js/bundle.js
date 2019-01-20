d290a61752cd99625ef3f7db9edfe62b2c7b547c
englishextra <englishextra@yandex.ru>
2019-01-20 09:20:20 +0300

    updates

--- libs/mytushino-muicss/js/bundle.js
+++ libs/mytushino-muicss/js/bundle.js
@@ -3271,7 +3271,7 @@ twttr, unescape, VK, WheelIndicator, Ya*/
   		"../../fonts/roboto-fontfacekit/2.137/css/roboto.css",
   		"../../fonts/roboto-mono-fontfacekit/2.0.986/css/roboto-mono.css",
   		"../../cdn/iframe-lightbox/0.2.8/css/iframe-lightbox.fixed.css",
-  		"../../cdn/img-lightbox/0.2.1/css/img-lightbox.fixed.css",
+  		"../../cdn/img-lightbox/0.2.3/css/img-lightbox.fixed.css",
   		"../../cdn/mui/0.9.39/css/mui.css"
   ]; */
 
@@ -3349,7 +3349,7 @@ twttr, unescape, VK, WheelIndicator, Ya*/
   		"../../cdn/ReadMore.js/1.0.0/js/readMoreJS.fixed.js",
   		"../../cdn/ripple-js/1.4.4/js/ripple.fixed.js",
   		"../../cdn/iframe-lightbox/0.2.8/js/iframe-lightbox.fixed.js",
-  		"../../cdn/img-lightbox/0.2.1/js/img-lightbox.fixed.js",
+  		"../../cdn/img-lightbox/0.2.3/js/img-lightbox.fixed.js",
   		"../../cdn/qrjs2/0.1.7/js/qrjs2.fixed.js",
   		"../../cdn/Tocca.js/2.0.1/js/Tocca.fixed.js",
   		"../../cdn/wheel-indicator/1.1.4/js/wheel-indicator.fixed.js",
